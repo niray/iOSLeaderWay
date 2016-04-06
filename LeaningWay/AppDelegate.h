08e021efc616a2f363c75d01de3046bcdf405a88
zhining.chen <cznc@qq.com>
2016-04-06 15:35:38 +0800

    Initial Commit

---
+++ LeaningWay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeaningWay
+//
+//  Created by Huway Mac on 16/4/6.
+//  Copyright © 2016年 Android Develope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
