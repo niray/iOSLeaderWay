dc4a7a18f6f7bd8f99aeae9654350cb09338a7da
zhining.chen <cznc@qq.com>
2016-05-21 18:54:19 +0800

    singleton

---
+++ BNRImageStore.h
@@ -0,0 +1,19 @@
+//
+//  BNRImageStore.h
+//  LeaningWay
+//
+//  Created by Huway Mac on 16/5/21.
+//  Copyright © 2016年 Android Develope. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <UIKit/UIKit.h>
+@interface BNRImageStore : NSObject
+
++(instancetype)sharedStore;
+
+-(void)setImage:(UIImage *)image forKey:(NSString *)key;
+-(UIImage *)imageForKey:(NSString *)key;
+-(void)deleteImageForKey:(NSString *)key;
+
+@end
---
+++ BNRImageStore.m
@@ -0,0 +1,59 @@
+//
+//  BNRImageStore.m
+//  LeaningWay
+//
+//  Created by Huway Mac on 16/5/21.
+//  Copyright © 2016年 Android Develope. All rights reserved.
+//
+
+#import "BNRImageStore.h"
+
+@interface BNRImageStore()
+
+@property (nonatomic,strong)NSMutableDictionary *dictinary;
+
+@end
+
+@implementation BNRImageStore
+
+
++(instancetype)sharedStore{
+    static BNRImageStore *sharedStore = nil;
+    if(!sharedStore){
+        sharedStore=[[self alloc]initPrivate];
+    }
+    return  sharedStore;
+}
+
+-(instancetype)init{
+    @throw[NSException exceptionWithName:@"Singleton" reason:@"Use +[BNRImageStore sharedStore]" userInfo:nil];
+    return  nil;
+}
+
+-(instancetype)initPrivate{
+    self = [super init];
+    if(self){
+        self.dictinary = [[NSMutableDictionary alloc]init];
+    }
+    return self;
+}
+
+-(void)setImage:(UIImage *)image forKey:(NSString*)key{
+    [self.dictinary setObject:image forKey:key];
+}
+
+-(UIImage *)imageForKey:(NSString*)key{
+    return [self.dictinary objectForKey:key];
+}
+
+-(void)deleteImageForKey:(NSString *)key{
+    if(!key){
+        return;
+    }
+    [self.dictinary removeObjectForKey:key];
+}
+
+
+
+
+@end
